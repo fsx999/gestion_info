9f26b64742e17f11ba0263af398c77c9aa83e4c6
paul guichon <paul.guichon@iedparis8.net>
2016-03-01 15:57:52 +0100

    migration duck

--- setup.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup
-
-setup(name='gestion_info', version='', packages=['gestion_info'], url='', license='', author='paul guichon',
-      author_email='paul.guichon@iedparis8.net', description='')
